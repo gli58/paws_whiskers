1b4b632ffa6a8e600acfa2dd1d9320fe0e05f71f
gavin <gli5@rrc.ca>
2025-11-26 16:25:22 -0600

    Add user account routes and organize route structure

--- config/routes.rb
+++ config/routes.rb
@@ -6,15 +6,23 @@ Rails.application.routes.draw do
 
   root 'products#index'
 
+  # Products and Categories
   resources :products, only: [ :index, :show ]
   resources :categories, only: [ :show ]
+
+  # Static Pages
   get 'pages/:slug', to: 'pages#show', as: 'page'
 
+  # Shopping Cart
   get 'cart', to: 'cart#show'
   post 'cart/add/:product_id', to: 'cart#add', as: 'add_to_cart'
   delete 'cart/remove/:product_id', to: 'cart#remove', as: 'remove_from_cart'
   patch 'cart/update_quantity/:product_id', to: 'cart#update_quantity', as: 'update_cart_quantity'
   delete 'cart/clear', to: 'cart#clear', as: 'clear_cart'
 
+  # Orders
   resources :orders, only: [ :new, :create, :show, :index ]
+
+  # User Account
+  resource :user, only: [ :show, :edit, :update ]
 end
