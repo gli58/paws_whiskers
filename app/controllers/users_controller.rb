a66c9386bf27b6ec35b4a16e0f50dd8801bd9dcc
gavin <gli5@rrc.ca>
2025-11-26 16:21:53 -0600

    Implement user account controller with show, edit, update actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,29 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_user
+
   def show
+    @orders = @user.orders.order(created_at: :desc).limit(5)
   end
 
   def edit
   end
 
   def update
+    if @user.update(user_params)
+      redirect_to user_path, success: "Profile updated successfully!"
+    else
+      render :edit, alert: "Failed to update profile"
+    end
+  end
+
+  private
+
+  def set_user
+    @user = current_user
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :phone, :street_address, :city, :province_id, :postal_code)
   end
 end
